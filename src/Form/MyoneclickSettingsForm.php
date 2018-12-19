47b2e71b80f3fe48a4b91e047bea9a116a0a79e8
devel7 <devel7@industrialmedia.com.ua>
2018-12-19 17:52:58 +0100

    Fix isAvailability

--- src/Form/MyoneclickSettingsForm.php
+++ src/Form/MyoneclickSettingsForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Serialization\Json;
-
+use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 
 class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectionInterface {
 
@@ -36,6 +36,46 @@ class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectio
       '#type' => 'vertical_tabs',
       '#title' => $this->t('Settings'),
     ];
+
+    // Tab - load_popup
+    $form['load_popup'] = [
+      '#type' => 'details',
+      '#title' => 'Вызов попап-формы',
+      '#group' => 'settings',
+      '#tree' => TRUE,
+    ];
+    $form['load_popup']['addtocart'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Add button "Buy one click" to add to cart form'),
+      '#default_value' => $settings_config->get('load_popup.addtocart'),
+      '#description' => $this->t('If the item is in stock and the price is not zero, add button "Buy one click" to add to cart form.'),
+    ];
+
+    /* @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface $entity_display_repository */
+    $entity_display_repository = \Drupal::service('entity_display.repository');
+    $view_modes = $entity_display_repository->getViewModes('commerce_product');
+    $options = [];
+    if ($view_modes) {
+      foreach ($view_modes as $key => $view_mode) {
+        $options[$key] = $view_mode['label'];
+      }
+    }
+    $options['full'] = t('Full');
+
+
+    $form['load_popup']['addtocart_view_modes'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Add to cart form for view modes'),
+      '#default_value' => !empty($settings_config->get('load_popup.addtocart_view_modes')) ? $settings_config->get('load_popup.addtocart_view_modes') : [],
+      '#options' => $options,
+      '#states' => [
+        'visible' => [
+          ':input[name="load_popup[addtocart]"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+
     // Tab - form
     $form['form'] = [
       '#type' => 'details',
@@ -117,11 +157,12 @@ class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectio
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $settings_config = $this->config('myoneclick.settings');
-    $settings_config
-      ->set('popup.dialog_options', $form_state->getValue('popup')['dialog_options'])
+    $settings_config->set('load_popup.addtocart', $form_state->getValue('load_popup')['addtocart'])
+      ->set('load_popup.addtocart_view_modes', array_filter($form_state->getValue('load_popup')['addtocart_view_modes']));
+    $settings_config->set('popup.dialog_options', $form_state->getValue('popup')['dialog_options'])
       ->set('popup.button_name', $form_state->getValue('popup')['button_name'])
-      ->set('popup.title', $form_state->getValue('popup')['title'])
-      ->set('form.name_label', $form_state->getValue('form')['name_label'])
+      ->set('popup.title', $form_state->getValue('popup')['title']);
+    $settings_config->set('form.name_label', $form_state->getValue('form')['name_label'])
       ->set('form.phone_label', $form_state->getValue('form')['phone_label'])
       ->set('form.mail_label', $form_state->getValue('form')['mail_label'])
       ->set('form.submit_label', $form_state->getValue('form')['submit_label'])
