6230b4b2846507acbe04f37596b100718b4f72cc
devel7 <devel7@industrialmedia.com.ua>
2019-04-10 16:44:48 +0200

    Add field city

--- src/Form/MyoneclickForm.php
+++ src/Form/MyoneclickForm.php
@@ -15,6 +15,9 @@ use Drupal\commerce_order\Resolver\OrderTypeResolverInterface;
 use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\commerce_shipping\PackerManagerInterface;
+
 
 class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
 
@@ -25,6 +28,13 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
    */
   protected $messenger;
 
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
 
   /**
    * The cart manager.
@@ -64,10 +74,20 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
 
 
   /**
+   * The packer manager.
+   *
+   * @var \Drupal\commerce_shipping\PackerManagerInterface
+   */
+  protected $packerManager;
+
+
+  /**
    * Constructs a new MyoneclickForm object.
    *
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    * @param \Drupal\commerce_cart\CartManagerInterface $cart_manager
    *   The cart manager.
    * @param \Drupal\commerce_cart\CartProviderInterface $cart_provider
@@ -78,14 +98,18 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
    *   The current store.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \Drupal\commerce_shipping\PackerManagerInterface $packer_manager
+   *   The packer manager.
    */
-  public function __construct(MessengerInterface $messenger, CartManagerInterface $cart_manager, CartProviderInterface $cart_provider, OrderTypeResolverInterface $order_type_resolver, CurrentStoreInterface $current_store, ConfigFactoryInterface $config_factory) {
+  public function __construct(MessengerInterface $messenger, EntityTypeManagerInterface $entity_type_manager, CartManagerInterface $cart_manager, CartProviderInterface $cart_provider, OrderTypeResolverInterface $order_type_resolver, CurrentStoreInterface $current_store, ConfigFactoryInterface $config_factory, PackerManagerInterface $packer_manager) {
     $this->messenger = $messenger;
+    $this->entityTypeManager = $entity_type_manager;
     $this->cartManager = $cart_manager;
     $this->cartProvider = $cart_provider;
     $this->orderTypeResolver = $order_type_resolver;
     $this->currentStore = $current_store;
     $this->configFactory = $config_factory;
+    $this->packerManager = $packer_manager;
   }
 
 
@@ -95,6 +119,8 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
   public static function create(ContainerInterface $container) {
     /* @var \Drupal\Core\Messenger\MessengerInterface $messenger */
     $messenger = $container->get('messenger');
+    /* @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = $container->get('entity_type.manager');
     /* @var \Drupal\commerce_cart\CartManagerInterface $cart_manager */
     $cart_manager = $container->get('commerce_cart.cart_manager');
     /* @var \Drupal\commerce_cart\CartProviderInterface $cart_provider */
@@ -105,14 +131,18 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
     $current_store = $container->get('commerce_store.current_store');
     /* @var \Drupal\Core\Config\ConfigFactoryInterface $config_factory */
     $config_factory = $container->get('config.factory');
+    /* @var \Drupal\commerce_shipping\PackerManagerInterface $packer_manager */
+    $packer_manager = $container->get('commerce_shipping.packer_manager');
 
     return new static(
       $messenger,
+      $entity_type_manager,
       $cart_manager,
       $cart_provider,
       $order_type_resolver,
       $current_store,
-      $config_factory
+      $config_factory,
+      $packer_manager
     );
   }
 
@@ -151,6 +181,12 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
       '#default_value' => $this->currentUser()->getEmail(),
       '#required' => TRUE,
     ];
+    $form['city'] = [
+      '#type' => 'textfield',
+      '#title' => $myoneclick_config->get('form.city_label'),
+      '#required' => TRUE,
+    ];
+
     $form['actions'] = [
       '#type' => 'actions',
     ];
@@ -167,6 +203,7 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
       ],
     ];
     $form['#attached']['html_head'][] = [$noindex_nofollow, 'noindex_nofollow'];
+    
     return $form;
   }
 
@@ -181,10 +218,13 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
     $product_id = $form_state->getValue('product_id');
     $name = $form_state->getValue('name');
     $phone = $form_state->getValue('phone');
     $mail = $form_state->getValue('mail');
+    $city = $form_state->getValue('city');
+
     /** @var \Drupal\commerce_product\Entity\Product $product */
     $product = Product::load($product_id);
     /** @var \Drupal\commerce_product\Entity\ProductVariation[] $product_variations */
@@ -200,9 +240,54 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
     }
     $this->cartManager->addOrderItem($cart, $order_item);
 
-    $cart->set('field_order_fio', $name);
-    $cart->set('field_order_phone', $phone);
+
     $cart->setEmail($mail);
+    $myoneclick_config = $this->configFactory->get('myoneclick.settings');
+    $save_fields = [
+      'save.fields.name',
+      'save.fields.phone',
+      'save.fields.city'
+    ];
+    foreach ($save_fields as $save_field) {
+      $value = NULL;
+      switch ($save_field) {
+        case 'save.fields.name':
+          $value = $name;
+          break;
+        case 'save.fields.phone':
+          $value = $phone;
+          break;
+        case 'save.fields.city':
+          $value = $city;
+          break;
+      }
+      if (!empty($value)) {
+        $save_field_value = $myoneclick_config->get($save_field);
+        if ($save_field_value) {
+          $save_field_value = explode('|', $save_field_value);
+          $field_name = $save_field_value[1];
+          if ($save_field_value[0] == 'commerce_order') {
+            $cart->set($field_name, $value);
+          }
+          elseif ($save_field_value[0] == 'profile') {
+            $shipping_profile = $this->getShippingProfile($cart);
+            $shipping_profile->set($field_name, $value);
+            /** @var \Drupal\commerce_shipping\Entity\ShipmentInterface[] $shipments */
+            $shipments = $cart->get('shipments')->referencedEntities();
+            list($shipments, $removed_shipments) = $this->packerManager->packToShipments($cart, $shipping_profile, $shipments);
+            $shipments[0]->setShippingProfile($shipping_profile);
+            $shipping_method = $this->getShippingMethod($cart);
+            $shipments[0]->setShippingMethod($shipping_method);
+            /** @var \Drupal\commerce_shipping\ShippingService $shipping_service */
+            $shipping_services = $shipping_method->getPlugin()->getServices();
+            $shipping_service = reset($shipping_services);
+            $shipments[0]->setShippingService($shipping_service->getId());
+            $cart->set('shipments', $shipments);
+          }
+        }
+      }
+    }
+
 
     $step_id = 'complete';
     $cart->set('checkout_step', $step_id);
@@ -218,6 +303,7 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
       'commerce_order' => $cart->id(),
       'step' => $step_id
     ]);
+
 
   }
 
@@ -237,6 +323,62 @@ class MyoneclickForm extends FormBase implements ContainerInjectionInterface {
       $transition = $order->getState()->getWorkflow()->getTransition('place');
       $order->getState()->applyTransition($transition);
     }
+  }
+
+
+  /**
+   * Gets the shipping profile.
+   *
+   * The shipping profile is assumed to be the same for all shipments.
+   * Therefore, it is taken from the first found shipment, or created from
+   * scratch if no shipments were found.
+   *
+   * @return \Drupal\profile\Entity\ProfileInterface
+   *   The shipping profile.
+   */
+  protected function getShippingProfile(\Drupal\commerce_order\Entity\OrderInterface $order) {
+    $shipping_profile = NULL;
+    /** @var \Drupal\commerce_shipping\Entity\ShipmentInterface $shipment */
+    foreach ($order->shipments->referencedEntities() as $shipment) {
+      $shipping_profile = $shipment->getShippingProfile();
+      break;
+    }
+    if (!$shipping_profile) {
+      $shipping_profile = $this->entityTypeManager->getStorage('profile')
+        ->create([
+          'type' => 'customer',
+          'uid' => $order->getCustomerId(),
+        ]);
+    }
+    return $shipping_profile;
+  }
+
+
+  protected function getShippingMethod(\Drupal\commerce_order\Entity\OrderInterface $order) {
+    $shipping_method = NULL;
+    /** @var \Drupal\commerce_shipping\Entity\ShipmentInterface $shipment */
+    foreach ($order->shipments->referencedEntities() as $shipment) {
+      $shipping_method = $shipment->getShippingMethod();
+      break;
+    }
+    /** @var \Drupal\commerce_shipping\ShippingMethodStorageInterface $shipping_method_storage */
+    $shipping_method_storage = $this->entityTypeManager->getStorage('commerce_shipping_method');
+    if (!$shipping_method) {
+      $myoneclick_config = $this->configFactory->get('myoneclick.settings');
+      $shipping_method_id = $myoneclick_config->get('save.shipping_method');
+      $shipping_method = $shipping_method_storage->load($shipping_method_id);
+    }
+    if (!$shipping_method) {
+      $shipping_methods = $shipping_method_storage->loadMultiple();
+      /** @var \Drupal\commerce_shipping\Entity\ShippingMethod $method */
+      foreach ($shipping_methods as $method) {
+        if ($method->isEnabled()) {
+          $shipping_method = $method;
+          break;
+        }
+      }
+    }
+    return $shipping_method;
   }
 
 
--- src/Form/MyoneclickSettingsForm.php
+++ src/Form/MyoneclickSettingsForm.php
@@ -7,8 +7,49 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Serialization\Json;
 use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\field\Entity\FieldStorageConfig;
 
 class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectionInterface {
+
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+
+  /**
+   * Constructs a \Drupal\system\ConfigFormBase object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($config_factory);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    /* @var \Drupal\Core\Config\ConfigFactoryInterface $config_factory */
+    $config_factory = $container->get('config.factory');
+    /* @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = $container->get('entity_type.manager');
+
+    return new static(
+      $config_factory,
+      $entity_type_manager
+    );
+  }
 
 
   /**
@@ -98,6 +139,11 @@ class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectio
       '#title' => $this->t('Mail label'),
       '#default_value' => $settings_config->get('form.mail_label'),
     ];
+    $form['form']['city_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('City label'),
+      '#default_value' => $settings_config->get('form.city_label'),
+    ];
     $form['form']['submit_label'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Submit label'),
@@ -136,8 +182,91 @@ class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectio
       '#show_nested' => FALSE,
       '#weight' => 90,
     );
+
+
+    // Tab - save
+    $form['save'] = [
+      '#type' => 'details',
+      '#title' => 'Сохранение заказа',
+      '#group' => 'settings',
+      '#tree' => TRUE,
+    ];
+    $options = [];
+    /** @var \Drupal\commerce_shipping\ShippingMethodStorageInterface $shipping_method_storage */
+    $shipping_method_storage = $this->entityTypeManager->getStorage('commerce_shipping_method');
+    $shipping_methods = $shipping_method_storage->loadMultiple();
+    /** @var \Drupal\commerce_shipping\Entity\ShippingMethod $shipping_method */
+    foreach ($shipping_methods as $shipping_method) {
+      if ($shipping_method->isEnabled()) {
+        $options[$shipping_method->id()] = $shipping_method->getName();
+      }
+    }
+    $form['save']['shipping_method'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Shipping method'),
+      '#options' => $options,
+      '#default_value' => $settings_config->get('save.shipping_method'),
+      '#required' => TRUE,
+      '#description' => 'Для модуля commerce_shipping способ доставки есть обязательным, значение по умолчанию',
+    ];
+    $form['save']['fields'] = [
+      '#type' => 'fieldset',
+      '#description' => '
+        В формате <strong>commerce_order|FIELDNAME</strong> или <strong>profile|FIELDNAME</strong> <br />
+        Примеры commerce_order|field_order_fio, profile|field_customer_city
+      ',
+    ];
+    $form['save']['fields']['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Save field: Name'),
+      '#default_value' => $settings_config->get('save.fields.name'),
+      '#element_validate' => [[get_class($this), 'saveFieldValidate']],
+    ];
+    $form['save']['fields']['phone'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Save field: Phone'),
+      '#default_value' => $settings_config->get('save.fields.phone'),
+      '#element_validate' => [[get_class($this), 'saveFieldValidate']],
+    ];
+    $form['save']['fields']['city'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Save field: City'),
+      '#default_value' => $settings_config->get('save.fields.city'),
+      '#element_validate' => [[get_class($this), 'saveFieldValidate']],
+    ];
+    
     return parent::buildForm($form, $form_state);
   }
+
+
+  public static function saveFieldValidate(&$element, FormStateInterface $form_state, &$complete_form) {
+    if (!empty($element['#value'])) {
+      $message = 'Не верный формат поля "' . $element['#title'] . '"';
+      $save_field = $element['#value'];
+      $save_field = explode('|', $save_field);
+      if (count($save_field) != 2) {
+        $form_state->setError($element, $message);
+        return;
+      }
+      if (!in_array($save_field[0], ['commerce_order', 'profile'])) {
+        $form_state->setError($element, $message);
+        return;
+      }
+      $save_field_1 = trim($save_field[1]);
+      if (empty($save_field_1)) {
+        $form_state->setError($element, $message);
+        return;
+      }
+      $field_config = FieldStorageConfig::loadByName($save_field[0], $save_field[1]);
+      if (empty($field_config)) {
+        $form_state->setError($element, $message);
+        return;
+      }
+    }
+  }
+
+
+
 
   /**
    * {@inheritdoc}
@@ -157,15 +286,25 @@ class MyoneclickSettingsForm extends ConfigFormBase implements ContainerInjectio
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $settings_config = $this->config('myoneclick.settings');
-    $settings_config->set('load_popup.addtocart', $form_state->getValue('load_popup')['addtocart'])
+    $settings_config
+      ->set('load_popup.addtocart', $form_state->getValue('load_popup')['addtocart'])
       ->set('load_popup.addtocart_view_modes', array_filter($form_state->getValue('load_popup')['addtocart_view_modes']));
-    $settings_config->set('popup.dialog_options', $form_state->getValue('popup')['dialog_options'])
+    $settings_config
+      ->set('popup.dialog_options', $form_state->getValue('popup')['dialog_options'])
       ->set('popup.button_name', $form_state->getValue('popup')['button_name'])
       ->set('popup.title', $form_state->getValue('popup')['title']);
-    $settings_config->set('form.name_label', $form_state->getValue('form')['name_label'])
+    $settings_config
+      ->set('form.name_label', $form_state->getValue('form')['name_label'])
       ->set('form.phone_label', $form_state->getValue('form')['phone_label'])
       ->set('form.mail_label', $form_state->getValue('form')['mail_label'])
+      ->set('form.city_label', $form_state->getValue('form')['city_label'])
       ->set('form.submit_label', $form_state->getValue('form')['submit_label'])
+      ->save();
+    $settings_config
+      ->set('save.shipping_method', $form_state->getValue('save')['shipping_method'])
+      ->set('save.fields.name', $form_state->getValue('save')['fields']['name'])
+      ->set('save.fields.phone', $form_state->getValue('save')['fields']['phone'])
+      ->set('save.fields.city', $form_state->getValue('save')['fields']['city'])
       ->save();
     parent::submitForm($form, $form_state);
   }
